e11012340e5a4e61b7e6c966760b2cc0f2e053c1
cjklein <cjklein@iastate.edu>
2017-01-22 16:41:05 -0600

    Added Item class

---
+++ PayItForward/PayItForward/Classes/Item.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PayItForward.Classes
+{
+    public class Item
+    {
+        #region Fields
+
+        private string _name;
+        private double _price;
+        private double _weight;
+
+        #endregion
+
+        public Item()
+        {
+            Name = "";
+            Price = 0.0;
+            Weight = 0.0;
+        }
+
+        public Item(string name, double price, double weight)
+        {
+            Name    = name;
+            Price   = price;
+            Weight  = weight;
+        }
+
+        #region Methods
+
+        public bool GetItem(string query)
+        {
+            return false;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public double Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        #endregion
+    }
+}
