20735c4f883e183ec8219de085edd4fdc6a1ce4d
Carter Thayer <carterwthayer@gmail.com>
2017-01-29 18:03:34 -0600

    Add a Category class based on the existing Item class.

---
+++ PayItForward/PayItForward/Classes/Category.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PayItForward.Classes
+{
+    public class Category
+    {
+        #region Fields
+
+        private string _name;
+        private int _id;
+
+        #endregion
+
+        public Category()
+        {
+            Name = "";
+            Id = 0;
+        }
+
+        public Category(int id, string name)
+        {
+            Name    = name;
+            Id = id;
+        }
+
+        #region Methods
+
+        public bool GetCategory(string query)
+        {
+            return false;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        #endregion
+    }
+}
