47b49a86f16fd8334b687a37d0ed4060c11a3c82
bcannon <bcannon@iastate.edu>
2017-04-27 23:59:50 -0500

    Removed admin home action tab

--- PayItForward/PayItForward/Pages/AdminProfile.aspx.designer.cs
+++ PayItForward/PayItForward/Pages/AdminProfile.aspx.designer.cs
@@ -13,33 +13,6 @@ namespace PayItForward.Pages {
     public partial class AdminProfile {
         
         /// <summary>
-        /// Generate control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Generate;
-        
-        /// <summary>
-        /// Remove control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Remove;
-        
-        /// <summary>
-        /// requestsGridView control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView requestsGridView;
-        
-        /// <summary>
         /// listPending control.
         /// </summary>
         /// <remarks>
